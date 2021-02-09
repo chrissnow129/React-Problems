1dc23df69d78fef036a47fa87f7ccc2b1dded50c
ChrisSnowman <csnowman129@gmail.com>
2021-02-08 22:42:17 -0800

    made the form

--- toDoList/src/components/App.js
+++ toDoList/src/components/App.js
@@ -1,37 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import todoListx from '../data';
 import 'tailwindcss/tailwind.css';
 
 export default function App(props) {
-	const [todoList, updateTodo] = useState([
-		{
-			title: 'Learn more about React',
-			completed: true
-		},
-		{
-			title: 'Write a new Component',
-			completed: false
-		},
-		{
-			title: 'Add some style',
-			completed: false
-		}
-	]);
+	const [todoList, updateTodo] = useState(todoListx);
+	const bodyInput = useRef(null);
+
+	const handleSubmit = evt => {
+		evt.preventDefault();
+		const bodyValue = bodyInput.current.value;
+		todoList.completed = 'false';
+		todoList.title = '';
+		console.log(todoList);
+		updateTodo([...todoList, bodyValue]);
+	};
 
 	return (
-		<div class="bg-green-100 mt-40 h-45 w-30 flex flex-col items-center justify-between">
-			{Object.keys(todoList).length
-				? todoList.map(todo => {
-						return (
-							<div class="w-72 h-18 mt-30 bg-green-200 rounded-lg shadow-md flex items-center text-center flex-col">
-								<h2>{todo.title}</h2>
-								<p>
-									{todo.completed == 'false' ? 'Completed' : 'Not Completed'}
-								</p>
-								<br />
-							</div>
-						);
-				  })
-				: ''}
-		</div>
+		<body>
+			<form onSubmit={handleSubmit} className="flex flex-col mt-14">
+				<label>
+					What do you Need To Do? <br />
+					<input type="text" ref={bodyInput} className="bg-gray-100" />
+				</label>{' '}
+				<br />
+				<input
+					type="submit"
+					value="Add To Do"
+					className="w-24 p-2 rounded-lg "
+				/>
+			</form>
+
+			<div className="bg-green-100 mt-40 h-65 w-25 p-8 flex items-center rounded-lg flex-wrap justify-between">
+				{Object.keys(todoList).length
+					? todoList.map(todo => {
+							return (
+								<div
+									className="
+							w-60 
+							h-23 
+							mt-30 
+							bg-green-200 
+							leading-7 
+							rounded-lg
+							shadow-sm 
+							flex 
+							items-center 
+							text-center 
+							flex-col 
+							justify-between
+							text-white
+							hover:shadow-md"
+								>
+									<h2 className="text-xl font-bold">{todo.title}</h2> <br />
+									<p>
+										{todo.completed == 'false' ? 'Completed' : 'Not Completed'}
+									</p>
+									<br />
+								</div>
+							);
+					  })
+					: ''}
+			</div>
+		</body>
 	);
 }
