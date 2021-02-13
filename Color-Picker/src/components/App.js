cb3af638a0e0c108897026c845eb17eea0ab3f18
ChrisSnowman <csnowman129@gmail.com>
2021-02-13 14:40:26 -0800

    Got RGB input to work, did more styling, and started on random color Scheme

--- Color-Picker/src/components/App.js
+++ Color-Picker/src/components/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import Rgb from './Rgb';
 import 'tailwindcss/tailwind.css';
 
 export default function App() {
+	////////// FOR RANDOM SINGLE COLORS ///////////
+
 	const [color, setColor] = useState();
 
 	const fetchColor = async () => {
@@ -14,7 +17,37 @@ export default function App() {
 				`http://www.thecolorapi.com/id?rgb=${r},${g},${b}`
 			);
 			const data = await response.json();
-			console.log(data); // just so we can look at it
+			console.log(data);
+			setColor(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchColor();
+	}, []);
+
+	const r2 = Math.floor(Math.random() * 256);
+	const g2 = Math.floor(Math.random() * 256);
+	const b2 = Math.floor(Math.random() * 256);
+	const rgb2 = [r2, g2, b2].join(', ');
+
+	/////// RANDOM COLOR SCHEME ///////
+
+	const [scheme, setScheme] = useState();
+
+	const fetchScheme = async () => {
+		const r = Math.floor(Math.random() * 256);
+		const g = Math.floor(Math.random() * 256);
+		const b = Math.floor(Math.random() * 256);
+
+		try {
+			const response = await fetch(
+				`http://www.thecolorapi.com/scheme?rgb=${r},${g},${b}&mode=complement`
+			);
+			const data = await response.json();
+			console.log(data);
 			setColor(data);
 		} catch (error) {
 			console.error(error);
@@ -27,31 +60,42 @@ export default function App() {
 
 	return (
 		<div>
-			<header className="h-40 bg-blue-200 static">
-				<h1 className="text-center m-auto text-5xl text-white static">
+			<header
+				style={{ backgroundColor: `rgb(${rgb2})` }}
+				className="mt-10 h-40 static"
+			>
+				<h1 className="text-center text-5xl text-white static">
 					Color Collection
 				</h1>
 			</header>
-			<div className="flex flex-col justify-center mt-10">
-				{color ? (
-					<>
-						<h1 className="text-center text-xl">Name: {color.name.value}</h1>
-						<h3 className="text-center text-md">
-							HEX Value: {color.hex.clean}
-						</h3>
-						<h3 className="text-center text-md">
-							RGB Value: {color.rgb.r}, {color.rgb.g}, {color.rgb.b}
-						</h3>
-						<img className="m-auto rounded-3xl" src={color.image.bare} />
-					</>
-				) : null}
-				<button
-					className="w-32 h-9 rounded-tl-lg rounded-br-lg bg-pink-100"
-					/*style={{backgroundColor:''}}*/ onClick={fetchColor}
+
+			{color ? (
+				<div
+					style={{
+						backgroundColor: `rgba(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b}, 0.3)`
+					}}
+					className="flex flex-col justify-center m-auto mt-10 w-1/4 h-64 rounded-xl"
 				>
-					Get New Color
-				</button>
-			</div>
+					<h1 className="text-center text-xl pt-2 text-white">
+						Name: {color.name.value}
+					</h1>
+					<h3 className="text-center text-md text-white">
+						HEX Value: {color.hex.clean}
+					</h3>
+					<h3 className="text-center text-md text-white">
+						RGB Value: {color.rgb.r}, {color.rgb.g}, {color.rgb.b}
+					</h3>
+					<img className="m-auto rounded-3xl" src={color.image.bare} />
+					<button
+						className="m-auto mt-2 w-32 h-9 rounded-tl-2xl rounded-br-2xl text-white"
+						style={{ backgroundColor: `${color.hex.value}` }}
+						onClick={fetchColor}
+					>
+						Get New Color
+					</button>
+				</div>
+			) : null}
+			<Rgb />
 		</div>
 	);
 }
---
+++ Color-Picker/src/components/Rgb.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+export default function Rgb(props) {
+	const [query, updateQuery] = useState({
+		baseURL: 'http://www.thecolorapi.com/id?',
+		option: 'rgb=',
+		rgb: '',
+		searchURL: ''
+	});
+	const [color, updtColor] = useState({});
+
+	const rgb = useRef(null);
+	// const b = useRef(null);
+	// const g = useRef(null);
+
+	const getRgb = async () => {
+		if (query.searchURL) {
+			try {
+				const response = await fetch(query.searchURL);
+				const data = await response.json();
+				await updtColor(data);
+				console.log(data);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	};
+
+	useEffect(() => {
+		getRgb();
+	}, [query]);
+
+	const handleSubmit = evt => {
+		evt.preventDefault();
+		updateQuery({
+			...query,
+			searchURL: query.baseURL + query.option + query.rgb
+		});
+	};
+
+	const handleChange = evt => {
+		updateQuery({ ...query, rgb: evt.target.value });
+	};
+	return (
+		<div>
+			<form className="flex flex-col" onSubmit={handleSubmit}>
+				<label>
+					R:{' '}
+					<input
+						className="bg-gray-200"
+						type="text"
+						onChange={handleChange}
+						value={query.rgb}
+						ref={rgb}
+					/>
+				</label>{' '}
+				<br />
+				{/* <label>
+					G:{' '}
+					<input
+						className="bg-gray-200"
+						type="text"
+						onChange={handleChange}
+						ref={g}
+					/>
+				</label>{' '}
+				<br />
+				<label>
+					R:{' '}
+					<input
+						className="bg-gray-200"
+						type="text"
+						onChange={handleChange}
+						ref={b}
+					/>
+				</label>{' '}
+				<br /> */}
+				<input className="w-32 h-9" type="submit" value="Get this Color" />
+			</form>
+		</div>
+	);
+}
