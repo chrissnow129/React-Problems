431b44a33b118123268ac84941c6bc3984d71fe5
ChrisSnowman <csnowman129@gmail.com>
2021-05-29 15:11:42 -0700

    deleted empty folders

--- toDoList/gulpfile.js
+++ toDoList/gulpfile.js
@@ -1,14 +1,7 @@
-
-// Explanation for Students ---- This is requires the gulp package from node modules
-// Gulp exports an object with many methods
-// task , watch, src and pipe will be the main ones we use today but see the gulp docs to expand and also see how you might refactor it to no longer use task and maybe use exports, series and parallells
-// i don't use them here because they are more magic and make it harder to show what's happening
 const gulp = require('gulp');
 
-// Explanation for Students ---- This is for compiling SASS, we haven't learned SASS yet but this is as good a chance as any to to talk about how we could compile it.
 const sass = require('gulp-sass');
 
-// Use Nodemon programatically
 const nodemon = require('gulp-nodemon');
 
 // Explanation for Students ---- This is for those pesky experimental features of css that are not available in all browsers without prefixes like webkit and moz
